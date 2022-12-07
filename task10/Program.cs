6f88226baa96b6ef10ab3858877545f367e563ca
Elena Korenko <elenakor987@gmail.com>
2022-12-07 16:57:41 +0300

    Task10

---
+++ task10/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая принимает на вход
+трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 
+*/
+
+Console.WriteLine("Введите число"); 
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+if (number>99 && number<1000)
+number=number/10;
+number=number%10;
+
+Console.WriteLine($"{number}");
+
